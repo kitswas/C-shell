678bd3a3e8fe8f3c2223cf559df0f4bc90e9fdc3
kitswas <90329875+kitswas@users.noreply.github.com>
2022-11-03 17:17:42 +0530

    first commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @brief Just trying out this setup
+ * @author Swastik Pal
+ * @version 0.1
+ * @date 2022-11-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ * Comments _must_ be written in [**Markdown**](https://www.markdownguide.org/cheat-sheet/).
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
