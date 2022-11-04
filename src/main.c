658668e63845eefd8cf9792529f4feb3d333bed9
kitswas <90329875+kitswas@users.noreply.github.com>
2022-11-04 08:58:15 +0530

    Basic I/O is in place

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -20,18 +21,47 @@
 int main()
 {
 	cls;
-	printf("Hello ");
-	printf("World!");
-	loadSettings();
-	//loop
-	//cleanup
+	// load config files, if any.
+	load_settings();
+	// loop
+	loop();
+	// cleanup
 	return 0;
 }
 
-int printprompt()
+int load_settings()
 {
 }
 
-int loadSettings()
+int loop()
 {
+	while (true)
+	{
+		print_prompt();
+		char *line = malloc(LINE_BUFFER_SIZE * sizeof(*line));
+		read_line(line, LINE_BUFFER_SIZE);
+		printf("You entered: %s\n", line); // debuggging only
+
+		free(line);
+	}
+}
+
+int print_prompt()
+{
+}
+
+int read_line(char *buffer, int buffer_size)
+{
+	int ch = 0;
+	int i = 0;
+	do
+	{
+		ch = getchar();
+		buffer[i] = ch;
+		++i;
+	} while (ch != '\n' && ch != EOF && i < buffer_size);
+	buffer[i - 1] = '\0'; // null terminate the string
+	while (ch != '\n' && ch != EOF) // discard the rest of the input line
+		ch = getchar();
+	return i;
 }
--- src/main.h
+++ src/main.h
@@ -11,6 +11,7 @@
 
 // macros here
 #define cls printf("\033[2J\033[1;1H"); // Clears the screen on both Windows 10 and Linux. Doesn't work on old Windows OSes
+#define LINE_BUFFER_SIZE 1024
 
 // global variables here
 
@@ -23,13 +24,27 @@
  *
  * Not yet implemented.
  */
-int loadSettings();
+int load_settings();
+
+/**
+ * @brief I keep looping, looping till it the user exits
+ *
+ * Implementation incomplete.
+ */
+int loop();
 
 /**
  * @brief Will print username\@machinename.
  *
  * Not yet implemented.
  */
-int printprompt();
+int print_prompt();
+
+/**
+ * @brief Reads a line from stdin.
+ *
+ * @return The number of characters read.
+ */
+int read_line(char *buffer, int buffer_size);
 
 #endif
