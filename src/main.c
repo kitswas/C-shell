kitswas/C-shell

commit e42d5a7a75989d44afbee0c34093175b6ff30343

Support multiple commands separated by ;

src/main.c
@@ -105,19 +105,29 @@ int loop()
 		read_line(line, line_size);
 		// printf("You entered: %s\n", line); // debuggging only
 
-		int nargs = 0;
-		char **args = parse_command(line, line_size, &nargs);
-		// for (size_t i = 0; i < nargs; i++) // debuggging only
-		// {
-		// 	printf("You entered: %s\n", args[i]);
-		// }
+		int ncmds = 0;
+		char **commands = parse_line(line, line_size, &ncmds);
 
-		int status = 0;
-		if (nargs > 0 && args[0] != NULL) // ignore empty lines and NULL commands
-			status = execute(nargs, args[0], args);
+		for (size_t nc = 0; nc < ncmds; ++nc)
+		{
+			char *command = commands[nc];
+			int nargs = 0;
+			char **args = parse_command(command, strlen(command), &nargs);
+
+			// printf("Command %ld: %s\n", nc, command); // debuggging only
+			// for (size_t i = 0; i < nargs; i++)		  // debuggging only
+			// {
+			// 	printf("Arg %ld: %s\n", i, args[i]);
+			// }
+
+			int status = 0;
+			if (nargs > 0 && args[0] != NULL) // ignore empty lines and NULL commands
+				status = execute(nargs, args[0], args);
+
+			free(args);
+		}
 
 		free(line);
-		free(args);
 	}
 }
 

src/parse.c
@@ -19,3 +19,21 @@ char **parse_command(char *command, size_t command_size, int *nargs)
 
 	return tokens;
 }
+
+char **parse_line(char *line, size_t line_size, int *ncmds)
+{
+	char **commands = malloc(line_size * sizeof(*commands));
+	char *command = strtok(line, COMMAND_DELIM);
+	int position = 0;
+
+	do
+	{
+		commands[position] = command;
+		++position;
+		command = strtok(NULL, COMMAND_DELIM);
+	} while (command != NULL);
+	*ncmds = position;
+	commands[position] = NULL; // NULL-terminate the list of strings
+
+	return commands;
+}

src/parse.h
@@ -14,6 +14,7 @@
 // macros here
 
 #define C_SHELL_TOK_DELIM " \t\r\n"
+#define COMMAND_DELIM ";"
 
 // global variables here
 
@@ -24,10 +25,21 @@
 /**
  * @brief Tokenizes the command.
  *
- * @param line A string.
- * @param line_size Length of the string.
+ * @param command A string.
+ * @param command_size Length of the string.
+ * @param nargs Stores the number of arguments.
  * @return char** An array of tokens.
  */
 char **parse_command(char *command, size_t command_size, int *nargs);
+
+/**
+ * @brief Splits the line into commands using ; as a delimiter.
+ *
+ * @param line A string.
+ * @param line_size Length of the string.
+ * @param ncmds Stores the number of commands.
+ * @return char** An array of commands.
+ */
+char **parse_line(char *line, size_t line_size, int *ncmds);
 
 #endif

commit b25e6d061321862b3292b5bafd2f1fdd0ec50e24

Fixed empty input crash bug
Bug introduced in commit e42d5a7a75989d44afbee0c34093175b6ff30343

src/main.c
@@ -104,9 +104,12 @@ int loop()
 		print_prompt();
 
 		size_t line_size = LINE_BUFFER_SIZE;
-		char *line = malloc(line_size * sizeof(*line));
+		char *line = calloc(line_size, sizeof(*line));
 		read_line(line, line_size);
 		// printf("You entered: %s\n", line); // debuggging only
+
+		if (line[0] == '\0')
+			continue; // ignore empty lines
 
 		int ncmds = 0;
 		char **commands = parse_line(line, line_size, &ncmds);
@@ -124,7 +127,7 @@ int loop()
 			// }
 
 			int status = 0;
-			if (nargs > 0 && args[0] != NULL) // ignore empty lines and NULL commands
+			if (nargs > 0 && args[0] != NULL) // ignore empty or NULL commands
 				status = execute(nargs, args[0], args);
 
 			if (status != 0)